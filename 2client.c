1f0d48b3042f9cbf03db97dccc55ad1f3c92aeaa
jiahuichen <jchen75@stevens.edu>
2017-04-28 23:09:44 -0400

    add solution

---
+++ 2client.c
@@ -0,0 +1,87 @@
+/*
+ * homework_6_solution_client.c
+ * CPE-555 Real-Time and Embedded Systems
+ * Instructor: Richard Prego
+ * Stevens Institute of Technology
+ * Fall 2016
+ */
+
+#include <stdio.h>
+#include <string.h>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <netdb.h>
+#include <unistd.h>
+
+#define PORT_NUM "5550"
+
+int main()
+{
+    struct addrinfo hints;
+    struct addrinfo *servinfo;
+    int sockfd;
+    char send_string[140];
+    char rcv_string[140];
+    int bytes;
+
+    /* clear hints structure */
+    memset(&hints, 0, sizeof(hints));
+
+    /* don't care IPv4 or IPv6
+     * TCP stream socket
+     * fill in local IP address automatically
+     */
+    hints.ai_family = AF_INET;
+    hints.ai_socktype = SOCK_STREAM;
+    hints.ai_flags = AI_PASSIVE;
+
+    /* getaddrinfo populates servinfo structure with server's address info */
+    if (getaddrinfo("localhost", PORT_NUM, &hints, &servinfo) != 0)
+    {
+        printf("Error in getaddrinfo\n");
+        return 1;
+    }
+
+    /* get a file descriptor for the new socket */
+    sockfd = socket(servinfo->ai_family, servinfo->ai_socktype, servinfo->ai_protocol);
+    if (sockfd == -1)
+    {
+        printf("Error creating socket\n");
+        return 1;
+    }
+
+    /* connect the socket */
+    if (connect(sockfd, servinfo->ai_addr, servinfo->ai_addrlen) == -1)
+    {
+        printf("Error connecting the socket\n");
+        return 1;
+    }
+
+    /* prompt user for some data to send */
+    printf("Enter string to send: ");
+    fgets(send_string, 140, stdin);
+
+    /* send the message on the socket */
+    bytes = send(sockfd, send_string, strlen(send_string), 0);
+    if (bytes == -1)
+    {
+        printf("Error sending message\n");
+    }
+    printf("Sent %d bytes to localhost port %s\n", bytes, PORT_NUM);
+
+    /* receive a message from the socket */
+    bytes = recv(sockfd, rcv_string, 140, 0);
+    if (bytes == -1)
+    {
+        printf("Error in recv()\n");
+        close(sockfd);
+        return 1;
+    }
+    rcv_string[bytes] = '\0';
+    printf("Received: %s\n", rcv_string);
+
+    /* close the socket */
+    close(sockfd);
+
+    return 0;
+}
---
+++ 2server.c
@@ -0,0 +1,118 @@
+/*
+ * homework_6_solution_server.c
+ * CPE-555 Real-Time and Embedded Systems
+ * Instructor: Richard Prego
+ * Stevens Institute of Technology
+ * Fall 2016
+ */
+
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <netdb.h>
+#include <unistd.h>
+
+#define PORT_NUM "5550"
+
+int main()
+{
+    struct addrinfo hints;
+    struct addrinfo *servinfo;
+    int sockfd;
+    int connected_sockfd;
+    struct addrinfo clientinfo;
+    socklen_t clientinfo_size;
+    char rcv_string[140];
+    int bytes;
+
+    /* clear hints structure */
+    memset(&hints, 0, sizeof(hints));
+
+    /* don't care IPv4 or IPv6
+     * TCP stream socket
+     * fill in local IP address automatically
+     */
+    hints.ai_family = AF_INET;
+    hints.ai_socktype = SOCK_STREAM;
+    hints.ai_flags = AI_PASSIVE;
+
+    /* getaddrinfo populates servinfo structure with local address info */
+    if (getaddrinfo(NULL, PORT_NUM, &hints, &servinfo) != 0)
+    {
+        printf("Error in getaddrinfo\n");
+        return 1;
+    }
+
+    /* get a file descriptor for the new socket */
+    sockfd = socket(servinfo->ai_family, servinfo->ai_socktype, servinfo->ai_protocol);
+    if (sockfd == -1)
+    {
+        printf("Error creating socket\n");
+        return 1;
+    }
+
+    /* bind the socket to the port we're interested in */
+    if (bind(sockfd, servinfo->ai_addr, servinfo->ai_addrlen) == -1)
+    {
+        printf("Error binding to port\n");
+        close(sockfd);
+        return 1;
+    }
+
+    /* listen for connections on the socket */
+    if (listen(sockfd, 10) == -1)
+    {
+        printf("Error in listen()\n");
+        close(sockfd);
+        return 1;
+    }
+    printf("Server is waiting for connections on port %s...\n", PORT_NUM);
+    
+    /* at this point we have received a connection
+     * so, we accept it
+     */
+    clientinfo_size = sizeof(clientinfo);
+    connected_sockfd = accept(sockfd, (struct sockaddr*)&clientinfo, &clientinfo_size);
+    if (connected_sockfd == -1)
+    {
+        printf("Error accepting connection\n");
+        return 1;
+    }
+    printf("Received connection from client\n");
+
+    /* receive a message from the socket */
+    bytes = recv(connected_sockfd, rcv_string, 140, 0);
+    if (bytes == -1)
+    {
+        printf("Error in recv()\n");
+        close(connected_sockfd);
+        close(sockfd);
+        return 1;
+    }
+    rcv_string[bytes] = '\0';
+    printf("Received: %s", rcv_string);
+
+    if (atoi(rcv_string) % 2 == 0)
+    {
+        /* even number */
+        bytes = send(connected_sockfd, "EVEN", 5, 0);
+    }
+    else
+    {
+        /* odd number */
+        bytes = send(connected_sockfd, "ODD", 4, 0);
+    }
+    if (bytes == -1)
+    {
+        printf("Error sending message\n");
+    }
+    printf("Sent %d bytes to port %s\n", bytes, PORT_NUM);
+
+    /* close the sockets */
+    close(connected_sockfd);
+    close(sockfd);
+
+    return 0;
+}
